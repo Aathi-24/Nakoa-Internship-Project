a6792f5235aab8821106f8425b0f66f9591670e1
Aathi-24 <aadithyavettrivel24@gmail.com>
2026-06-10 16:13:23 +0530

    Commit 2

---
+++ IP_Reputation_Checker/services/main file.py
@@ -0,0 +1,131 @@
+import requests
+import pandas as pd
+import json
+from datetime import datetime
+
+def abuseipdb(ip):
+    API_KEY = "0ef8bc21a09b3512c27bad77fb63602851a760ed70efb89b49be2eba0c0e3fb30ac3ca8098cdc17f"
+    url = "https://api.abuseipdb.com/api/v2/check"
+
+    headers = {
+        "Key": API_KEY,
+        "Accept": "application/json"
+    }
+
+    params = {
+        "ipAddress": ip,
+        "maxAgeInDays": 90,
+        "verbose": ""
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params
+    )
+
+    if response.status_code == 200:
+        data = response.json()
+        score = data["data"]["abuseConfidenceScore"]
+        reports = data["data"]["totalReports"]
+
+        if score >= 50 and reports >= 10:
+            status = "Blocked"
+        else:
+            status = "Safe"
+        
+        if(status == "Blocked"):
+            if score == 0:
+                reason = "Clean"
+            elif score < 50:
+                reason = "Suspicious"
+            else:
+                reason = "Malicious"
+        else:
+            reason = "Nil"
+        
+        last_reported = data["data"].get("lastReportedAt")
+        if last_reported:
+            date = last_reported.split("T")[0]
+        else:
+            date = "Nil"
+
+        dict = {
+            "Vendor" : "AbuseIPDB",
+            "Blocked" : status,
+            "Reason" : reason,
+            "Total Reports" : data["data"]["totalReports"],
+            "Last Reported" : date
+        }
+        return dict
+    
+    else:
+        print("Error:", response.status_code)
+        print(response.text)
+        return None
+    
+def virustotal(ip):
+    API_KEY = "1840ec0af3668a612149e6b3aae3895f7048b52de22d3ba2018059f1f01b3b95"
+
+    url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}"
+
+    headers = {
+        "x-apikey": API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        attributes = data["data"]["attributes"]
+
+        timestamp = attributes.get("last_modification_date")
+        if timestamp:
+            last_reported = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d")
+        else:
+            last_reported = "Nil"
+
+        stats = attributes["last_analysis_stats"]
+        total_reports = (stats.get("malicious", 0) + stats.get("suspicious", 0))
+
+        vendor_results = attributes.get("last_analysis_results",{})
+        final_results = []
+
+        for vendor, result in vendor_results.items():
+            category = result.get("category", "").lower()
+            if category in ["malicious", "suspicious"]:
+                blocked = "Blocked"
+                reason = result.get("result",category.title())
+
+            else:
+                blocked = "Safe"
+                reason = "Nil"
+
+            final_results.append(
+                {
+                    "Vendor": vendor,
+                    "Blocked": blocked,
+                    "Reason": reason,
+                    "Total Reports": total_reports,
+                    "Last Reported": last_reported,
+                }
+            )
+        return final_results
+    
+    else:
+        print(f"Error for {ip}: {response.status_code}")
+        return None
+    
+if __name__ == "__main__":
+    ip = input("Enter an IP : ")
+    result = []
+    abip = abuseipdb(ip)
+    result.append(abip)
+
+    vtres = virustotal(ip)
+    for vt in vtres:
+        result.append(vt)
+
+    df = pd.DataFrame(result, index = (range(1,len(result)+1)))
+    df.to_csv("Reports_of_IP.csv", mode = "w", index = False)
+    print(df)
